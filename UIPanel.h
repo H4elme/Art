cde35a8d088d27bcee1a5b1458ae5d4ff3ae4aa0
H4elme <atmrosh@gmail.com>
2025-06-14 16:02:58 +0200

    Add base abstract class for UI elements, add UI panel class

---
+++ UIPanel.h
@@ -0,0 +1,21 @@
+#ifndef UIPANEL_H
+#define UIPANEL_H
+
+#include "raylib.h"
+#include "UIElement.h"
+#include <vector>
+
+class UIPanel : public UIElement {
+    int x, y, width, height;
+    Color backgroundColor;
+    std::vector<UIElement*> elements;
+public:
+    UIPanel(int x_, int y_, int width_, int height_, Color backgroundColor_);
+    void addElement(UIElement* newElement);
+    void draw() override;
+    void click() override;
+    bool hover() override;
+    ~UIPanel();
+};
+
+#endif
